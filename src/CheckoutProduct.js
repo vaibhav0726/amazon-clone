f0342ed36f84d26f976de737ed17d28b98319233
vaibhav0726 <vaibhav.varshney2_cs20@gla.ac.in>
2022-06-30 01:24:06 +0530

    added remove from basket functionality

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -8,6 +8,8 @@ function CheckoutProduct({id, image, title, price, rating}) {
     const removeFromBasket = () =>{
         //remove the item from the basket
         dispatch({
+            type: "REMOVE_FROM_BASKET",
+            id: id,
             
         })
 
--- src/reducer.js
+++ src/reducer.js
@@ -15,6 +15,29 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             };
+        
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            // copying basket state
+            // important to learn 3:18:24 🚀🚀
+            let newBasket = [...state.basket]; 
+
+            if(index >= 0){
+                // slicing the basket and giving the first one
+                newBasket.splice(index, 1); 
+            }
+            else{
+                console.warn(
+                    `Cant remove product (id: ${action.id}) as its not in basket!`
+                )
+            }
+
+            return{
+                ...state,
+                basket: newBasket
+            }
 
       default:
         return state;
